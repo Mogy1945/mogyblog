40214ce57366f6c82741f68cfd461730f46736dd
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-21 16:30:00 +0900

    done 404page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
+import styles from '../styles/404.module.scss';
+import Link from "next/link";
+
 export default function Custom404() {
     return (
-        <main className="main">
-            <p>ページがありません。</p>
+        <main className={styles.main}>
+            <h3>404 - not found -</h3>
+            <p>指定されたページが存在しません。<br className={styles.sp} />URLを見直してみてください。</p>
+
+            <Link href="/">
+                <span>ホームに戻る</span>
+            </Link>
         </main>
     );
 }
