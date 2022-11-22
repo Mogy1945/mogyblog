cfb044a1f216e72e72c1bda0053268ec808a2d0d
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-22 10:47:58 +0900

    change to export default

--- libs/client.js
+++ libs/client.js
@@ -1,6 +1,8 @@
-import { createClient } from 'microcms-js-sdk';
+import { createClient } from 'microcms-js-sdk'
 
-export const client = createClient({
-    serviceDomain: 'uw4bprzt79',
-    apiKey: process.env.API_KEY,
-});
+const client = createClient({
+  serviceDomain: 'uw4bprzt79',
+  apiKey: process.env.API_KEY,
+})
+
+export default client
