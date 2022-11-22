f42a01437100471a5e1834b2e34e87cc9dbcff4f
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-22 14:40:30 +0900

    add About page

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+
+export default function About() {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <p>から</p>
+      </main>
+    </>
+  )
+}
