cd8522c283959697edfddb125b07cfb43615ce3a
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-22 15:37:03 +0900

    add current tab

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,20 @@
 import Link from 'next/link'
 import styles from './Header.module.scss'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <ul className={styles.ul}>
-          <li>
+          <li className={router.pathname === '/' ? 'current' : ''}>
             <Link href="/">Blog</Link>
           </li>
-          <li>
+          <li className={router.pathname === '/About' ? 'current' : ''}>
             <Link href="/About">About</Link>
           </li>
-          <li>
+          <li className={router.pathname === '/Contact' ? 'current' : ''}>
             <Link href="/Contact">Contact</Link>
           </li>
         </ul>
