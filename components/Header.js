96c30f3e6010ae14d68b48e1035fa22f4dd1ffde
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-22 10:47:02 +0900

    create link

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
                     <li><Link href="/">Blog</Link></li>
-                    <li><Link href="/">Skil-Set</Link></li>
-                    <li><Link href="/">Contact</Link></li>
+                    <li><Link href="/skill-set">Skil-Set</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
