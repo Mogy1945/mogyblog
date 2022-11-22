1dfd3e67871a5a4aed1e85c3d2240a53936ebdc3
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-22 10:51:36 +0900

    add category

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -42,6 +42,9 @@ export default function BlogId({ blog }) {
         <main className={styles.main}>
             <h1 className={styles.title}>{blog.title}</h1>
             <p className={styles.publishedAt}>{blog.publishedAt}</p>
+            {blog.category.map((category)=>{
+                return <p key={category.id} className='category'>{category.name}</p>
+            })}
             <div
                 dangerouslySetInnerHTML={{
                     __html: `${blog.body}`,
