4253ad8cd7854c60b09e1a84eac7146b66c1f555
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-24 13:28:47 +0900

    update home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import client from '../libs/client'
 import styles from '../styles/Home.module.scss'
 import Header from '../components/Header'
@@ -10,7 +11,7 @@ import Header from '../components/Header'
  *  -getStaticProps ⇒ ビルド時に一度だけ実行
  *  -blog ⇒ blog[{title:...,body...,・・・},{title:...,・・・}]の形でデータが格納されている
  */
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const data = await client.get({ endpoint: 'blog' })
 
   return {
@@ -34,12 +35,18 @@ export default function Home({ blog }) {
       <main className={styles.main}>
         <h3>Blog</h3>
         <p>
-          【サイト利用技術】Next.js、SSG、SCSS、microCMS、nodemailer、Vercel
+          【サイト利用技術】Next.js、SSR、SCSS、microCMS、nodemailer、Vercel
         </p>
         <div>
           <ul className={styles.blogContainer}>
             {blog.map((blogDate) => (
               <li key={blogDate.id}>
+                <Image
+                  height={400}
+                  width={400}
+                  alt="サムネイル画像です"
+                  src={blogDate.sumbnail.path.url}
+                />
                 <Link href={`/blog/${blogDate.id}`}>
                   <span>{blogDate.title}</span>
                 </Link>
