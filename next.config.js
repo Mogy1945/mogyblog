46ad3593eaa8c070755aad2111801d34f9b663df
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-29 10:47:24 +0900

    congig update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io', 'source.unsplash.com'],
   },
+  basePath: '/public',
 }
 
 module.exports = nextConfig
