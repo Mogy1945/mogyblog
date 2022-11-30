858af58197e7c56cef7acc61eec1a348e5a0b08d
Mogy1945 <hatano.shun@shanon.co.jp>
2022-11-30 15:06:38 +0900

    change: contact system change to Formspree

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,32 +1,50 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import { useForm, ValidationError } from '@formspree/react'
+import Link from 'next/link'
 import Header from '../components/Header'
 import styles from '../styles/Contact.module.scss'
 
 export default function Form() {
+  const [state, handleSubmit] = useForm('mdojrabp')
   const [Name, setName] = useState('')
   const [Email, setEmail] = useState('')
   const [Message, setMessage] = useState('')
-  const [modalFlag, setmodalFlag] = useState(false)
 
-  // mail_api setting
-  const registerUser = async (event) => {
-    event.preventDefault()
-    const res = await fetch('/api/send', {
-      body: JSON.stringify({
-        name: Name,
-        email: Email,
-        message: Message,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-
-    await res.json()
+  if (state.succeeded) {
+    return (
+      <>
+        <Head>
+          <title>Contact | Mogy-Blog</title>
+        </Head>
+        <main className={styles.main}>
+          <h3>Thank you for your Contact!</h3>
+          <p>
+            以下の内容でお問合せを完了いたしました。返信までしばらくお待ちください。
+          </p>
+          <div className={styles.formContainer}>
+            <form>
+              <div className={styles.box}>
+                <p className={styles.titleResult}>お名前</p>
+                <p className={styles.result}>{Name}</p>
+              </div>
+              <div className={styles.box}>
+                <p className={styles.titleResult}>E-mailアドレス</p>
+                <p className={styles.result}>{Email}</p>
+              </div>
+              <div className={styles.box}>
+                <p className={styles.titleResult}>お問い合わせ内容</p>
+                <p className={styles.result}>{Message}</p>
+              </div>
+            </form>
+          </div>
+          <Link href="/" className={styles.bottomLink}>
+            記事一覧に戻る
+          </Link>
+        </main>
+      </>
+    )
   }
-
   return (
     <>
       <Head>
@@ -39,10 +57,11 @@ export default function Form() {
           お問い合わせの際は、以下項目を記入し、「送信」ボタンを押してください。
         </p>
         <div className={styles.formContainer}>
-          <form onSubmit={registerUser}>
+          <form onSubmit={handleSubmit}>
             <div className={styles.box}>
               <p className={styles.title}>お名前</p>
               <input
+                id="name"
                 name="name"
                 type="text"
                 placeholder="例）山田太郎"
@@ -54,57 +73,46 @@ export default function Form() {
             <div className={styles.box}>
               <p className={styles.title}>E-mailアドレス</p>
               <input
-                name="email"
+                id="email"
                 type="email"
+                name="email"
                 placeholder="例）sample@test.co.jp"
                 onBlur={(e) => {
                   setEmail(e.target.value)
                 }}
               />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
             </div>
             <div className={styles.box}>
               <p className={styles.title}>お問い合わせ内容</p>
               <textarea
+                id="message"
+                name="message"
                 placeholder="例）どうしてエンジニアになろうと思ったのですか？"
-                name="text"
                 onBlur={(e) => {
                   setMessage(e.target.value)
                 }}
               ></textarea>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
             </div>
             <div className={styles.box}>
               <input
                 type="submit"
+                disabled={state.submitting}
                 className={styles.submit}
                 value="送信"
-                onClick={() => {
-                  setmodalFlag(true)
-                }}
               />
             </div>
           </form>
         </div>
-        {modalFlag ? (
-          <>
-            <div className={styles.modalContainer}>
-              <p className={styles.modalTitle}>Success!</p>
-              <p className={styles.modalText}>
-                送信しました！返信まで少々お待ちください。
-              </p>
-              <button
-                className={styles.modalButton}
-                onClick={() => {
-                  setmodalFlag(false)
-                }}
-              >
-                閉じる
-              </button>
-            </div>
-            <div className={styles.modalBg}></div>
-          </>
-        ) : (
-          ''
-        )}
       </main>
     </>
   )
